d9c7750636a8ccc6dadb32df1d8480898bc1fe43
Noah King <noahking123@yahoo.com>
2022-07-20 21:49:24 -0400

    commit tree cc79024c72f1e21397e59fb7dc31c37a546108a1

---
+++ index.js
@@ -0,0 +1,66 @@
+function shout(string) {
+    return string.toUpperCase();
+}
+
+function whisper(string){
+    return string.toLowerCase();
+}
+
+function logShout(string){
+    console.log(string.toUpperCase());
+}
+
+function logWhisper(string){
+    console.log(string.toLowerCase());
+}
+
+function sayHiToHeadphonedRoommate(string) {
+    if (string === string.toLowerCase())
+    return "I can't hear you!"
+
+    else if (string === string.toUpperCase())
+    return "YES INDEED!"
+
+    else if (string = "Let's have dinner together!")
+    return "I would love to!"
+}
+/*
+function sayHiToHeadphonedRoommate(string) {
+    if (string === string.toUpperCase());
+    return "YES INDEED!";
+
+}
+*/
+
+/*function sayHiToHeadphonedRoommate(string){
+    console.log(string);
+    console.log(string.toLowerCase);
+    string1 = console.log(string) === console.log(string.toLowerCase());
+    if string1 = true;
+    console.log("I can't hear you!");
+   /* else if string1 === false;
+    console.log("YES INDEED!");
+    else string1 === "Let's have dinner together!";
+    console.log("I would love to!");
+}*/
+
+/*function sayHiToHeadphonedRoommate(string) {
+    string = true;
+    string ? ("I can''t Hear You!") : ("YES INDEED!");
+    console.log(string);
+}*/
+
+/*function sayHiToHeadphonedRoommate(string) {
+    const upperCase = string.toUpperCase();
+
+    const lowerCase = string.toLowerCase();
+
+    if (string = upperCase) {
+        console.log("YES INDEED!");
+}   else if (string = lowerCase) {
+        console.log("I can't hear you!");
+}
+}*/
+
+
+
